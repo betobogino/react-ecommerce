51bab2c53714ae5009a2591d69a0b499f64f9135
betoCFH <beto.bogino@gmail.com>
2022-08-15 17:12:39 -0300

    First commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+
+function Card(){
+    return (
+        <div className="card">
+            <h1>Title</h1> 
+            <p> 
+                Hola lalal alalalala lalaaaaaaaaaaaaa aaaaaa a a aaaaaa aaaaaaaaa aaaaaaaa aaaaaaaaa aaaaa aaaaaa aaaaaaaa a aaaaaaa aaa aaaaa aaaaaaaaa aaaaaaaaaaaaa aaaa aaaaaaaaaaaaaaaa a aaaaa aaaa aaaaa aaaaaaa aaaaa aaaaa aa  aaaa aaaaaa aaaaaaa aaa aaa a a aaaaaaaaaaaa aaaaaaa aaaaaaaa
+            </p>
+            <Button variant="outline-success">Aceptar</Button>
+        </div>
+    );
+}
+
+export default Card;
