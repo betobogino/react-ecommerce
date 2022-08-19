ac5686bb7338df6e33b8e8810325c981b799c5a6
betoCFH <beto.bogino@gmail.com>
2022-08-18 21:41:20 -0300

    Navbar listo para entregar

--- src/components/NavDropdown.jsx
+++ src/components/NavDropdown.jsx
@@ -1,42 +1,41 @@
 import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const itemsDropdown = [
+	'Vehiculos',
+	'Inmuebles',
+	'Supermercado',
+	'Tecnologia',
+	'Hogar y Muebles',
+	'Electrodomesticos',
+	'Herramientas',
+	'Construccion',
+	'Deportes y Fitness',
+	'Accesorios para Vehiculos',
+	'Moda',
+	'Juegos y Juguetes',
+	'Bebés',
+	'Belleza y Cuidado Personal',
+	'Salud y Equipamiento Médico',
+	'Industrias y Oficinas',
+	'Agro',
+	'Productos Sustentables',
+	'Servicios',
+	'Más vendidos',
+	'Tiendas oficiales',
+	'Ver más categorías',
+];
+
+const navsItemsDropdown = itemsDropdown.map((items, index) => {
+	return (
+		<NavDropdown.Item href='#action/3.1' key={index}>
+			{items}
+		</NavDropdown.Item>
+	);
+});
 function navDropdown() {
 	return (
 		<NavDropdown title='Categorias' id='basic-nav-dropdown'>
-			<NavDropdown.Item href='#action/3.1'>Vehiculos</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.2'>Inmuebles</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Supermercado</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Tecnologia</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Hogar y Muebles</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.1'>Electrodomesticos</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.2'>Herramientas</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Construccion</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Deportes y Fitness</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>
-				Accesorios para Vehiculos
-			</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Moda</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Juegos y Juguetes</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Bebés</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>
-				Belleza y Cuidado Personal
-			</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>
-				Salud y Equipamiento Médico
-			</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>
-				Industrias y Oficinas
-			</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Agro</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>
-				Productos Sustentables
-			</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Servicios</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Más vendidos</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Tiendas oficiales</NavDropdown.Item>
-			<NavDropdown.Item href='#action/3.3'>Ver más categorías</NavDropdown.Item>
-
-			<NavDropdown.Divider />
-			<NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
+			{navsItemsDropdown}
 		</NavDropdown>
 	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,35 @@
+import { NavLink } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import NavDropdown from './NavDropdown';
 
-function navbar() {
-	const links = [
-		'Ofertas',
-		'Historial',
-		'Supermercado',
-		'Moda',
-		'Vender',
-		'Ayuda',
-	];
+const links = [
+	'Ofertas',
+	'Historial',
+	'Supermercado',
+	'Moda',
+	'Vender',
+	'Ayuda',
+];
 
+const navLinks = links.map((link, index) => {
+	return <NavLink key={index}>{link}</NavLink>;
+});
+function navbar() {
 	return (
-		<Navbar bg='light' expand='lg'>
-			<Container>
-				<Navbar.Brand href='#home'>React-Ecommerce</Navbar.Brand>
+		<Container>
+			<Navbar bg='light' expand='lg'>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
 					<Nav className='me-auto'>
 						<NavDropdown></NavDropdown>
-						{links.forEach((link) => {
-							<Nav.Link href='#link'>{link}</Nav.Link>;
-						})}
+						<Nav>{navLinks}</Nav>
 					</Nav>
 				</Navbar.Collapse>
-			</Container>
-		</Navbar>
+			</Navbar>
+		</Container>
 	);
 }
 
